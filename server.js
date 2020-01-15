74445587d42073801b3702f9207c1d366f91816d
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-15 10:51:54 -0500

    added html route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 // Listener; Starts server
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
