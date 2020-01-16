46a99d1df85ab8ebd6f5b3c505977900f49b1ad2
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-15 20:09:36 -0500

    reordered get requests

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,15 @@
 // Dependencies
 const path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-     // If no matching route is found default to index.html
-    app.get ("*", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+    app.get("/notes", function (req, res) {
+        console.log("in notes html");
+        res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
 
-    app.get ("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"))
-    })
+    // If no matching route is found default to index.html
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 }
