db210de2643d08ddedd8ac6c926c711844759b0b
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-19 17:55:36 -0500

    added route to style.css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,15 @@
 const path = require("path");
 
 module.exports = function (app) {
-
+    // route to notes.html
     app.get("/notes", function (req, res) {
-        console.log("in notes html");
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
     
+    // route to css
+    app.get("/assets/css/styles.css", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"))
+    });
 
     // If no matching route is found default to index.html
     app.get("*", function (req, res) {
