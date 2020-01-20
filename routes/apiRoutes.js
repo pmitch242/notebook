24161b187a0241b59daad00ac59dcdbb2fda4e33
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-19 19:19:48 -0500

    created post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,24 @@
 const db = require("../db/db");
 
+
 module.exports = function (app){
+    let num = 0;
+
     app.get("/api/notes", function(req,res){
         res.json(db);
     });
 
     app.post("/api/notes", function(req, res){
-        console.log(req.body);
-        // db.push(res.body);
-        res.end("ok");
+        num++;
+        let newNote = req.body;
+        newNote.id = num;
+        console.log(newNote)
+        db.push(newNote);
+        res.json(newNote);
+    })
+
+    app.delete("/api/notes/:id", function(req, res){
+        let deleteID = req.params.id;
+        console.log
     })
 }
